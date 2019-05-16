981612c50dc6d7bd59ef1c20c41f29b344814003
Zorica Babić <zorica.b96@gmail.com>
2019-05-17 00:44:21 +0200

    Calendar component is created.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,41 +1,43 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { PacijenComponent } from './components/pacijent/pacijent.component';
-import { MaterijalComponent } from './components/materijal/materijal.component';
-import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
-import { StrukaComponent } from './components/struka/struka.component';
-import { SlikaComponent } from './components/slika/slika.component';
-import { DijagnozaComponent } from './components/dijagnoza/dijagnoza.component';
-import { OrdinacijaComponent } from './components/ordinacija/ordinacija.component';
-import { ArtiklComponent } from './components/artikl/artikl.component';
-import { ZaposleniComponent } from './components/zaposleni/zaposleni.component';
-import { IsplataComponent } from './components/isplata/isplata.component';
-import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
-import { RadnoMestoComponent } from './components/radno-mesto/radno-mesto.component';
-import { StatusComponent } from './components/status/status.component';
-import { VrstaIntervencijeComponent } from './components/vrsta-intervencije/vrsta-intervencije.component';
-import { StavkaPorudzbineComponent } from './components/stavka-porudzbine/stavka-porudzbine.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PacijenComponent } from "./components/pacijent/pacijent.component";
+import { MaterijalComponent } from "./components/materijal/materijal.component";
+import { DobavljacComponent } from "./components/dobavljac/dobavljac.component";
+import { StrukaComponent } from "./components/struka/struka.component";
+import { SlikaComponent } from "./components/slika/slika.component";
+import { DijagnozaComponent } from "./components/dijagnoza/dijagnoza.component";
+import { OrdinacijaComponent } from "./components/ordinacija/ordinacija.component";
+import { ArtiklComponent } from "./components/artikl/artikl.component";
+import { ZaposleniComponent } from "./components/zaposleni/zaposleni.component";
+import { IsplataComponent } from "./components/isplata/isplata.component";
+import { PorudzbinaComponent } from "./components/porudzbina/porudzbina.component";
+import { RadnoMestoComponent } from "./components/radno-mesto/radno-mesto.component";
+import { StatusComponent } from "./components/status/status.component";
+import { VrstaIntervencijeComponent } from "./components/vrsta-intervencije/vrsta-intervencije.component";
+import { StavkaPorudzbineComponent } from "./components/stavka-porudzbine/stavka-porudzbine.component";
+import { KalendarComponent } from "./components/kalendar/kalendar.component";
 
 const routes: Routes = [
-  { path: 'pacijent', component: PacijenComponent},
-  { path: 'artikl', component: ArtiklComponent},
-  { path: 'struka', component: StrukaComponent},
-  { path: 'slika', component: SlikaComponent},
-  { path: 'ordinacija', component: OrdinacijaComponent},
-  { path: 'dobavljac', component: DobavljacComponent},
-  { path: 'dijagnoza', component: DijagnozaComponent},
-  { path: 'materijal', component: MaterijalComponent},
-  { path: 'zaposleni', component: ZaposleniComponent},
-  { path: 'isplata', component: IsplataComponent},
-  { path: 'porudzbina', component: PorudzbinaComponent},
-  { path: 'radno-mesto', component: RadnoMestoComponent},
-  { path: 'status', component: StatusComponent},
-  { path: 'vrstaIntervencije', component: VrstaIntervencijeComponent},
-  { path: 'stavkaPorudzbine', component: StavkaPorudzbineComponent}
+  { path: "pacijent", component: PacijenComponent },
+  { path: "artikl", component: ArtiklComponent },
+  { path: "struka", component: StrukaComponent },
+  { path: "slika", component: SlikaComponent },
+  { path: "ordinacija", component: OrdinacijaComponent },
+  { path: "dobavljac", component: DobavljacComponent },
+  { path: "dijagnoza", component: DijagnozaComponent },
+  { path: "materijal", component: MaterijalComponent },
+  { path: "zaposleni", component: ZaposleniComponent },
+  { path: "isplata", component: IsplataComponent },
+  { path: "porudzbina", component: PorudzbinaComponent },
+  { path: "radno-mesto", component: RadnoMestoComponent },
+  { path: "status", component: StatusComponent },
+  { path: "vrstaIntervencije", component: VrstaIntervencijeComponent },
+  { path: "stavkaPorudzbine", component: StavkaPorudzbineComponent },
+  { path: "kalendar", component: KalendarComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,53 +25,53 @@ import {
   MatSelectModule
 } from "@angular/material";
 import { PacijentService } from "./services/pacijent.service";
-import { PacijenComponent } from './components/pacijent/pacijent.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterijalComponent } from './components/materijal/materijal.component';
-import { OrdinacijaComponent } from './components/ordinacija/ordinacija.component';
-import { DobavljacComponent } from './components/dobavljac/dobavljac.component';
-import { DijagnozaComponent } from './components/dijagnoza/dijagnoza.component';
-import { SlikaComponent } from './components/slika/slika.component';
-import { StrukaComponent } from './components/struka/struka.component';
-import { ArtiklComponent } from './components/artikl/artikl.component';
-import { ArtiklService } from './services/artikl.service';
-import { DobavljacService } from './services/dobavljac.service';
-import { DijagnozaService } from './services/dijagnoza.service';
-import { OrdinacijaService } from './services/ordinacija.service';
-import { StrukaService } from './services/struka.service';
-import { MaterijalService } from './services/materijal.service';
-import { TableComponent } from './table/table.component';
-import { PacijentDialogComponent } from './components/dialogs/pacijent-dialog/pacijent-dialog.component';
-import { MaterijalDialogComponent } from './components/dialogs/materijal-dialog/materijal-dialog.component';
-import { DobavljacDialogComponent } from './components/dialogs/dobavljac-dialog/dobavljac-dialog.component';
-import { DijagnozaDialogComponent } from './components/dialogs/dijagnoza-dialog/dijagnoza-dialog.component';
-import { OrdinacijaDialogComponent } from './components/dialogs/ordinacija-dialog/ordinacija-dialog.component';
-import { SlikaDialogComponent } from './components/dialogs/slika-dialog/slika-dialog.component';
-import { StrukaDialogComponent } from './components/dialogs/struka-dialog/struka-dialog.component';
-import { ArtiklDialogComponent } from './components/dialogs/artikl-dialog/artikl-dialog.component';
-import { FormsModule } from '@angular/forms';
-import { IsplataComponent } from './components/isplata/isplata.component';
-import { IsplataDialogComponent } from './components/dialogs/isplata-dialog/isplata-dialog.component';
-import { ZaposleniDialogComponent } from './components/dialogs/zaposleni-dialog/zaposleni-dialog.component';
-import { ZaposleniComponent } from './components/zaposleni/zaposleni.component';
-import { IsplataService } from './services/isplata.service';
-import { ZaposleniService } from './services/zaposleni.service';
-import { PorudzbinaDialogComponent } from './components/dialogs/porudzbina-dialog/porudzbina-dialog.component';
-import { PorudzbinaComponent } from './components/porudzbina/porudzbina.component';
-import { PorudzbinaService } from './services/porudzbina.service';
-import { RadnoMestoDialogComponent } from './components/dialogs/radno-mesto-dialog/radno-mesto-dialog.component';
-import { RadnoMestoComponent } from './components/radno-mesto/radno-mesto.component';
-import { RadnoMestoService } from './services/radno-mesto.service';
-import { StatusDialogComponent } from './components/dialogs/status-dialog/status-dialog.component';
-import { StatusComponent } from './components/status/status.component';
-import { StatusService } from './services/status.service';
-import { VrstaIntervencijeComponent } from './components/vrsta-intervencije/vrsta-intervencije.component';
-import { VrstaIntervencijeDialogComponent } from './components/dialogs/vrsta-intervencije-dialog/vrsta-intervencije-dialog.component';
-import { VrstaIntervencijeService } from './services/vrsta-intervencije.service';
-import { StavkaPorudzbineComponent } from './components/stavka-porudzbine/stavka-porudzbine.component';
-import { StavkaPorudzbineDialogComponent } from './components/dialogs/stavka-porudzbine-dialog/stavka-porudzbine-dialog.component';
-import { StavkaPorudzbineService } from './services/stavka-porudzbine.service';
-
+import { PacijenComponent } from "./components/pacijent/pacijent.component";
+import { HttpClientModule } from "@angular/common/http";
+import { MaterijalComponent } from "./components/materijal/materijal.component";
+import { OrdinacijaComponent } from "./components/ordinacija/ordinacija.component";
+import { DobavljacComponent } from "./components/dobavljac/dobavljac.component";
+import { DijagnozaComponent } from "./components/dijagnoza/dijagnoza.component";
+import { SlikaComponent } from "./components/slika/slika.component";
+import { StrukaComponent } from "./components/struka/struka.component";
+import { ArtiklComponent } from "./components/artikl/artikl.component";
+import { ArtiklService } from "./services/artikl.service";
+import { DobavljacService } from "./services/dobavljac.service";
+import { DijagnozaService } from "./services/dijagnoza.service";
+import { OrdinacijaService } from "./services/ordinacija.service";
+import { StrukaService } from "./services/struka.service";
+import { MaterijalService } from "./services/materijal.service";
+import { TableComponent } from "./table/table.component";
+import { PacijentDialogComponent } from "./components/dialogs/pacijent-dialog/pacijent-dialog.component";
+import { MaterijalDialogComponent } from "./components/dialogs/materijal-dialog/materijal-dialog.component";
+import { DobavljacDialogComponent } from "./components/dialogs/dobavljac-dialog/dobavljac-dialog.component";
+import { DijagnozaDialogComponent } from "./components/dialogs/dijagnoza-dialog/dijagnoza-dialog.component";
+import { OrdinacijaDialogComponent } from "./components/dialogs/ordinacija-dialog/ordinacija-dialog.component";
+import { SlikaDialogComponent } from "./components/dialogs/slika-dialog/slika-dialog.component";
+import { StrukaDialogComponent } from "./components/dialogs/struka-dialog/struka-dialog.component";
+import { ArtiklDialogComponent } from "./components/dialogs/artikl-dialog/artikl-dialog.component";
+import { FormsModule } from "@angular/forms";
+import { IsplataComponent } from "./components/isplata/isplata.component";
+import { IsplataDialogComponent } from "./components/dialogs/isplata-dialog/isplata-dialog.component";
+import { ZaposleniDialogComponent } from "./components/dialogs/zaposleni-dialog/zaposleni-dialog.component";
+import { ZaposleniComponent } from "./components/zaposleni/zaposleni.component";
+import { IsplataService } from "./services/isplata.service";
+import { ZaposleniService } from "./services/zaposleni.service";
+import { PorudzbinaDialogComponent } from "./components/dialogs/porudzbina-dialog/porudzbina-dialog.component";
+import { PorudzbinaComponent } from "./components/porudzbina/porudzbina.component";
+import { PorudzbinaService } from "./services/porudzbina.service";
+import { RadnoMestoDialogComponent } from "./components/dialogs/radno-mesto-dialog/radno-mesto-dialog.component";
+import { RadnoMestoComponent } from "./components/radno-mesto/radno-mesto.component";
+import { RadnoMestoService } from "./services/radno-mesto.service";
+import { StatusDialogComponent } from "./components/dialogs/status-dialog/status-dialog.component";
+import { StatusComponent } from "./components/status/status.component";
+import { StatusService } from "./services/status.service";
+import { VrstaIntervencijeComponent } from "./components/vrsta-intervencije/vrsta-intervencije.component";
+import { VrstaIntervencijeDialogComponent } from "./components/dialogs/vrsta-intervencije-dialog/vrsta-intervencije-dialog.component";
+import { VrstaIntervencijeService } from "./services/vrsta-intervencije.service";
+import { StavkaPorudzbineComponent } from "./components/stavka-porudzbine/stavka-porudzbine.component";
+import { StavkaPorudzbineDialogComponent } from "./components/dialogs/stavka-porudzbine-dialog/stavka-porudzbine-dialog.component";
+import { StavkaPorudzbineService } from "./services/stavka-porudzbine.service";
+import { KalendarComponent } from "./components/kalendar/kalendar.component";
 
 @NgModule({
   declarations: [
@@ -107,13 +107,26 @@ import { StavkaPorudzbineService } from './services/stavka-porudzbine.service';
     VrstaIntervencijeComponent,
     VrstaIntervencijeDialogComponent,
     StavkaPorudzbineComponent,
+    StavkaPorudzbineDialogComponent,
+    KalendarComponent
+  ],
+  entryComponents: [
+    PacijentDialogComponent,
+    MaterijalDialogComponent,
+    DobavljacDialogComponent,
+    DijagnozaDialogComponent,
+    SlikaDialogComponent,
+    OrdinacijaDialogComponent,
+    PorudzbinaDialogComponent,
+    StrukaDialogComponent,
+    ArtiklDialogComponent,
+    IsplataDialogComponent,
+    ZaposleniDialogComponent,
+    RadnoMestoDialogComponent,
+    StatusDialogComponent,
+    VrstaIntervencijeDialogComponent,
     StavkaPorudzbineDialogComponent
   ],
-  entryComponents: [PacijentDialogComponent, MaterijalDialogComponent,
-    DobavljacDialogComponent, DijagnozaDialogComponent, SlikaDialogComponent,
-    OrdinacijaDialogComponent, PorudzbinaDialogComponent, StrukaDialogComponent,
-    ArtiklDialogComponent, IsplataDialogComponent, ZaposleniDialogComponent, RadnoMestoDialogComponent, StatusDialogComponent,
-    VrstaIntervencijeDialogComponent, StavkaPorudzbineDialogComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -137,11 +150,23 @@ import { StavkaPorudzbineService } from './services/stavka-porudzbine.service';
     MatNativeDateModule,
     MatSelectModule
   ],
-  providers: [PacijentService, ArtiklService, PorudzbinaService, DobavljacService,
-    DijagnozaService, OrdinacijaService, StrukaService, MaterijalService,
-    ZaposleniService, IsplataService, PorudzbinaService, RadnoMestoService, StatusService, VrstaIntervencijeService,
-     StavkaPorudzbineService],
+  providers: [
+    PacijentService,
+    ArtiklService,
+    PorudzbinaService,
+    DobavljacService,
+    DijagnozaService,
+    OrdinacijaService,
+    StrukaService,
+    MaterijalService,
+    ZaposleniService,
+    IsplataService,
+    PorudzbinaService,
+    RadnoMestoService,
+    StatusService,
+    VrstaIntervencijeService,
+    StavkaPorudzbineService
+  ],
   bootstrap: [AppComponent]
-
 })
 export class AppModule {}
